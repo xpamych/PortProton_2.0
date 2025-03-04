e2d3b0218ddcf65c483c3c276ec70d6a25383eb0
Mikhail Tergoev <fidel@altlinux.org>
2025-03-05 01:12:00 +0300

    added run script

--- portproton.py
+++ portproton.py
@@ -5,6 +5,11 @@ from modules.env_var import *
 from modules.files_worker import *
 from modules.downloader import *
 
+data_path = get_env_var("DATA_PATH")
+work_path = get_env_var("WORK_PATH")
+
 if __name__ == "__main__":
 
-    log.info("Привет мир!")
+    log.info(data_path)
+    log.info(work_path)
+    log.info(sys.argv[1:])

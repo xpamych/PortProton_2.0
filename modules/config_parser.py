9aa5c5bab98d4118e5214bb4cfcdd25b4e461d0e
Mikhail Tergoev <fidel@altlinux.org>
2025-03-07 02:11:57 +0300

    added exception for json parser

--- modules/config_parser.py
+++ modules/config_parser.py
@@ -6,9 +6,9 @@ def var(var_name):
     py_work_path = get_env_var("PYTHON_WORK_PATH")
 
     # Читаем конфигурационный файл
-    with open(py_work_path + "/config.json", "r") as file:
-        config = json.load(file)
-    if config[var_name]:
-        return config[var_name]
-    else:
-        log.critical(f"в config.json нет значения переменной: {var_name}")
+    try:
+        with open(py_work_path + "/config.json", "r") as file:
+            config = json.load(file)
+            return config[var_name]
+    except Exception as e:
+        log.critical(f"в config.json нет значения переменной: {e}")
